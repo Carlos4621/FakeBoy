490c71c532479b15ce6ab867e7836ee38826cd6f
Carlos Iván <carlosivanhb@hotmail.com>
2025-03-04 12:41:01 -0600

    Añadido test para LD R, u8

---
+++ test/test_CPU/test_LD_R_u8.cpp
@@ -0,0 +1,40 @@
+#include "Cartridge.hpp"
+#include "CPU.hpp"
+#include <gtest/gtest.h>
+#include <iostream>
+
+static constexpr std::string_view CartidgePath{ "NoMBC_LD_R_u8_Test_ROM.gb" };
+static constexpr uint16_t MinimumTCyclesNeeded{ 216 };
+
+static constexpr std::array ExpectedValues {
+    0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFE, 0xFF
+};
+
+class CPU_LD_R_u8_Test : public ::testing::Test {
+protected:
+    Cartridge cartridge_m{ CartidgePath };
+    VideoRAM videoRAM_m;
+    WorkingRAM workingRAM_m;
+    HighRAM highRAM_m;
+    EchoRAM echoRAM_m{ &workingRAM_m };
+
+    MemoryBus memorybus_m{ &cartridge_m, &workingRAM_m, &highRAM_m, &echoRAM_m, &videoRAM_m };
+
+    CPU cpu_m{ &memorybus_m };
+
+    void SetUp() override {
+        for (size_t i{ 0 }; i < MinimumTCyclesNeeded; ++i) {
+            cpu_m.processTCycle();
+        }
+     }
+};
+
+TEST_F(CPU_LD_R_u8_Test, LD_R_R_OpcodesWorks) {
+    for (size_t i{ 0 }; i < ExpectedValues.size(); ++i) {
+        const auto address{ 0xA000 + i };
+
+        const auto value{ memorybus_m.read(address) };
+
+        EXPECT_EQ(value, ExpectedValues[i]);
+    }
+}
