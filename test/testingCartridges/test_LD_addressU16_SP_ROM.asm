0c5c63f446a66ba0820636e3786b0e63cebc382b
Carlos Iván <carlosivanhb@hotmail.com>
2025-03-11 09:36:13 -0600

    Añadido opcode LD (u16), SP junto a sus tests

---
+++ test/testingCartridges/test_LD_addressU16_SP_ROM.asm
@@ -0,0 +1,45 @@
+SECTION "Header", ROM0[$100]
+
+    nop
+    jp Start
+
+    db $CE,$ED,$66,$66,$CC,$0D,$00,$0B
+    db $03,$73,$00,$83,$00,$0C,$00,$0D
+    db $00,$08,$11,$1F,$88,$89,$00,$0E
+    db $DC,$CC,$6E,$E6,$DD,$DD,$D9,$99
+    db $BB,$BB,$67,$63,$6E,$0E,$EC,$CC
+    db $DD,$DC,$99,$9F,$BB,$B9,$33,$3E
+
+    db "LD (u16), SP",0,0,0,0
+
+    db "00"
+
+    db $00  
+
+    db $00 ; Tipo de MBC
+
+    db $00 ; Tamaño de la ROM
+
+    db $02 ; Tamaño de la RAM
+
+    db $01
+
+    db $00
+
+    db $00  
+
+    db $00  
+
+    dw $0000  
+
+Start:
+
+    ld sp, $BBAA
+    ld [$A000], sp
+
+    ld sp, $DDCC
+    ld [$A002], sp
+
+    jp Start
+Loop:
+    jr Loop
